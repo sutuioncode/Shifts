fa2f0aa0f371d1a736674577c9a26572c8742368
Socrates Utui <socratesutui@live.com>
2025-01-14 03:56:53 +0200

    activation hot module replacement (HMR) for backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,11 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: "0.0.0.0",
+    port: 8080, // Ensure this matches the exposed port
+    hmr: {
+      host: 'localhost', // or your Docker host IP
+    },
+  },
 })
