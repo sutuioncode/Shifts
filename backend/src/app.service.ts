e9e1b530ea87a42902f59a5a6a0899318df30d0b
Socrates Utui <socratesutui@live.com>
2025-01-16 01:34:38 +0200

    added: list all shifts service

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { Shift } from '@prisma/client';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!' ; 
+  }
+
+  listShifts(): Promise<Shift[]>{
+    return this.prisma.shift.findMany();
   }
 }
