213267cafb76434903eb9a915923067ca0825cad
Socrates Utui <socratesutui@live.com>
2025-01-16 02:46:28 +0200

    added:list shifts api

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Shift } from '@prisma/client';
 
-@Controller()
+@Controller('shifts')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('list-shifts')
+  listShifts(): Promise<Shift[]> {
+    return this.appService.listShifts()
   }
 }
